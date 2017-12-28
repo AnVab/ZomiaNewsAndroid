bc691460475541863c30bb900500f545b14309f9
Andrey <anv.devmobile@gmail.com>
2017-12-28 17:17:06 +0300

    Renamed layouts. Added fragments.

--- app/src/main/java/news/zomia/zomianews/fragments/LoginFragment.java
+++ app/src/main/java/news/zomia/zomianews/fragments/LoginFragment.java
@@ -19,7 +19,7 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.layout_login, container, false);
+        return inflater.inflate(R.layout.layout_feeds_list, container, false);
     }
 
     @Override
