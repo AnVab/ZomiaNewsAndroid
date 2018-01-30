f367271db028335f99ef07ed06e01a342a920692
AnVab <anv.devmobile@gmail.com>
2018-01-30 23:38:53 +0300

    comment

--- app/src/main/java/news/zomia/zomianews/data/model/TagFeedPair.java
+++ app/src/main/java/news/zomia/zomianews/data/model/TagFeedPair.java
@@ -5,7 +5,9 @@ import android.arch.persistence.room.Embedded;
 /**
  * Created by Andrey on 28.01.2018.
  */
-
+/*
+ Class is used to show Tag and Feed pair from db in Join table.
+* */
 public class TagFeedPair {
 
     @Embedded
