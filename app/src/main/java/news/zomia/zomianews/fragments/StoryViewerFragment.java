da726e36b007c88ee63648d7b236032a88a05c0d
AnVab <anv.devmobile@gmail.com>
2018-03-23 00:21:32 +0300

    Added loading web view content through JSoup. Using CSS and custom font.

--- app/src/main/java/news/zomia/zomianews/fragments/StoryViewerFragment.java
+++ app/src/main/java/news/zomia/zomianews/fragments/StoryViewerFragment.java
@@ -6,22 +6,29 @@ import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Timestamp;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -33,6 +40,8 @@ import news.zomia.zomianews.customcontrols.OnSwipeTouchListener;
 import news.zomia.zomianews.data.viewmodel.StoryViewModel;
 import news.zomia.zomianews.data.viewmodel.StoryViewModelFactory;
 import news.zomia.zomianews.di.Injectable;
+
+import static android.webkit.WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -52,13 +61,15 @@ public class StoryViewerFragment extends Fragment
     @Inject
     StoryViewModelFactory storyViewModelFactory;
     private StoryViewModel storyViewModel;
+    @Inject
+    SharedPreferences sharedPref;
 
     SwipeRefreshLayout swipeRefreshLayout;
     WebView storyPageViewer;
     private Long dateTimestamp;
     private String title;
     private String content;
-
+    private String storyDateText;
     public StoryViewerFragment() {
         // Required empty public constructor
     }
@@ -95,12 +106,16 @@ public class StoryViewerFragment extends Fragment
 
         storyPageViewer = (WebView) view.findViewById(R.id.storyPageViewer );
         storyPageViewer.getSettings().setJavaScriptEnabled(true);
+        storyPageViewer.getSettings().setDomStorageEnabled(true);
+        storyPageViewer.getSettings().setAllowFileAccess(true);
+        storyPageViewer.getSettings().setAllowFileAccessFromFileURLs(true);
+        storyPageViewer.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        storyPageViewer.getSettings().setLayoutAlgorithm(TEXT_AUTOSIZING);
+
         storyPageViewer.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         storyPageViewer.setScrollbarFadingEnabled(true);
         storyPageViewer.getSettings().setLoadsImagesAutomatically(true);
-        //storyPageViewer.getSettings().setLoadWithOverviewMode(true);
-        //storyPageViewer.getSettings().setUseWideViewPort(true);
-        //storyPageViewer.getSettings().setMinimumFontSize(40);
+
 
         storyPageViewer.setOnTouchListener(new OnSwipeTouchListener(getActivity())
         {
@@ -143,10 +158,7 @@ public class StoryViewerFragment extends Fragment
                 }
             }
         });
-
-        //loadContent();
     }
-
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -163,14 +175,11 @@ public class StoryViewerFragment extends Fragment
                     content = resource.data.getContent();
 
                     loadContent();
-
-                    swipeRefreshLayout.setRefreshing(false);
                 }
                 else
                 {
                     swipeRefreshLayout.setRefreshing(false);
                 }
-
         });
     }
 
@@ -187,9 +196,14 @@ public class StoryViewerFragment extends Fragment
             //Convert timestamp to milliseconds format
             Timestamp tmp = new Timestamp(dateTimestamp / 1000);
             Date dateToStr = new Date(tmp.getTime());
-            String dateToText = formatter.format(dateToStr);
+            storyDateText = formatter.format(dateToStr);
+            String targetUrl = "";
+            String serverAddress = sharedPref.getString(getString(R.string.preferences_serverAddress), getString(R.string.preferences_serverAddress_default));
+            String serverPort = sharedPref.getString(getString(R.string.preferences_serverPort), getString(R.string.preferences_serverPort_default));
 
-            storyPageViewer.loadDataWithBaseURL("", getStyledFont(title, dateToText, content), "text/html", "UTF-8", "");
+            targetUrl = "http://" + serverAddress + ":" + serverPort + "/storage/" + content;
+            Log.d("ZOMIA", "baseurl: " + targetUrl);
+            new GetPage(targetUrl).execute();
         }
     }
 
@@ -197,6 +211,52 @@ public class StoryViewerFragment extends Fragment
     public void onRefresh() {
         if(storyPageViewer != null) {
             storyPageViewer.reload();
+        }
+    }
+
+    private void injectCSS() {
+        try {
+            InputStream inputStream = getActivity().getAssets().open("pagestyles.css");
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            inputStream.close();
+
+            String en = "" +
+                    "@font-face {\n" +
+                    "font-family: OpenSans-Bold;\n" +
+                    "src: url('file:///android_asset/fonts/OpenSans-Bold.ttf')\n" +
+                    "}\n" +
+                    "\n" +
+                    "body {\n" +
+                    "font-family: OpenSans-Bold;\n" +
+                    "font-size: medium;\n" +
+                    "text-align: justify;\n" +
+                    "}\n" +
+                    "\n" +
+                    "img{\n" +
+                    "display: inline;\n" +
+                    "height: auto;\n" +
+                    "max-width: 100%;\n" +
+                    "}\n" +
+                    "\n" +
+                    "h2 {\n" +
+                    "text-align: justify;\n" +
+                    "}\n" +
+                    "\n" +
+                    "h6 {\n" +
+                    "text-align: left;\n" +
+                    "}\n";
+            String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
+            storyPageViewer.loadUrl("javascript:(function() {" +
+                    "var parent = document.getElementsByTagName('head').item(0);" +
+                    "var style = document.createElement('style');" +
+                    "style.type = 'text/css';" +
+                    // Tell the browser to BASE64-decode the string into your script !!!
+                    "style.innerHTML = window.atob('" + encoded + "');" +
+                    "parent.appendChild(style)" +
+                    "})()");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -250,5 +310,39 @@ public class StoryViewerFragment extends Fragment
     public interface OnStoryViewerListener {
         public void nextStoryRequest(Integer currentStoryId);
         public void goBackToStoriesList();
+    }
+
+    private class GetPage extends AsyncTask<Void, Void, Void> {
+        private String url;
+        private String dataBody;
+        public GetPage(String url)
+        {
+            this.url = url;
+            this.dataBody = "";
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Connect to the web site
+                Document document = Jsoup.parse(new URL(url).openStream(), "CP1251", url);
+                dataBody = document.body().toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            //Set data
+            storyPageViewer.loadDataWithBaseURL("", getStyledFont(title, storyDateText, dataBody), "text/html", "UTF-8", "");
+            swipeRefreshLayout.setRefreshing(false);
+        }
     }
 }
