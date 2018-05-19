7c1f2a5b69113f2bae43202aea15e2e7464839a8
AnVab <anv.devmobile@gmail.com>
2018-05-20 00:35:24 +0300

    Action bar color.

--- app/src/main/java/news/zomia/zomianews/MainActivity.java
+++ app/src/main/java/news/zomia/zomianews/MainActivity.java
@@ -27,6 +27,7 @@ import android.content.res.Resources;
 import android.os.Build;
 import android.support.design.widget.AppBarLayout;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -118,6 +119,7 @@ public class MainActivity extends AppCompatActivity
         ActionBar actionBar = getSupportActionBar();
         // Enable the Up button
         actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setBackgroundDrawable( ContextCompat.getDrawable(this, R.drawable.action_bar_color));
 
         ((ZomiaApp) getApplication()).setUnauthorizedInterceptorListener(this);
         ((ZomiaApp) getApplication()).setNetworkConnectionInterceptorListener(this);
